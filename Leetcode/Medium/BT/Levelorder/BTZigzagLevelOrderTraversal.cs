085124933c1172a120e7fdd11a88e53fbd01c1d1
HackPoint <genashm@ge.com>
2025-06-16 22:23:09 +0300

    chore: 103. Binary Tree Zigzag Level Order Traversal (medium)

--- Leetcode/Medium/BT/Levelorder/BTZigzagLevelOrderTraversal.cs
+++ Leetcode/Medium/BT/Levelorder/BTZigzagLevelOrderTraversal.cs
@@ -2,21 +2,28 @@ using Leetcode.Easy.LinkedList.DFS;
 
 namespace Leetcode.Medium.BT.LevelOrder;
 
-public class BTLevelOrderTraversal
+public class BTZigzagLevelOrderTraversal
 {
     private IList<IList<int>> _result = new List<IList<int>>();
-    public IList<IList<int>> LevelOrder(TreeNode root) {
+    
+    public IList<IList<int>> ZigzagLevelOrder(TreeNode root) {
         int level = 0;
         Dfs(root, level);
         return _result;
     }
-
+    
     private void Dfs(TreeNode node,int level) {
         if(node == null) return;
         if(level == _result.Count) _result.Add(new List<int>());
-        // visit
-        _result[level].Add(node.val);
-        
+
+        if(level % 2 == 0) {
+            _result[level].Add(node.val);
+        } 
+
+        if(level % 2 == 1) {
+            _result[level].Insert(0, node.val);
+        }
+
         Dfs(node.left, level + 1);
         Dfs(node.right, level + 1);
     }
