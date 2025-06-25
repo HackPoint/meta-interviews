cdf7074e50b203a335012c13da64c88238b71d62
HackPoint <genashm@ge.com>
2025-06-26 00:41:25 +0300

    chore: 530. Minimum Absolute Difference in BST (easy)

---
+++ Leetcode/Easy/BT/InOrder/MinimumAbsoluteDifferenceInBst.cs
@@ -0,0 +1,33 @@
+using Leetcode.Easy.LinkedList.DFS;
+
+namespace Leetcode.Easy.BT.InOrder;
+
+public class MinimumAbsoluteDifferenceInBst
+{
+    public int GetMinimumDifference(TreeNode root)
+    {
+        int minDiff = int.MaxValue;
+        int? prev;
+
+        void InOrder(TreeNode node)
+        {
+            if (node == null) return;
+
+            // Left
+            InOrder(node.left);
+
+            // Node
+            if (prev != null)
+                minDiff = System.Math.Min(minDiff, node.val - prev.Value);
+
+            prev = node.val;
+
+            // Right
+            InOrder(node.right);
+        }
+
+        InOrder(root);
+        return minDiff;
+    }
+
+}
